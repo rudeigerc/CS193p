6b937c4189bd71b8d1488a7237fb9475090a3505
rudeigerc <rudeigerc@gmail.com>
2016-11-28 19:30:57 +0800

    finish requirement 2

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -18,7 +18,11 @@ class ViewController: UIViewController {
 		let digit = sender.currentTitle!
 		if userIsInTheMiddleOfTyping {
 			let textCurrentlyInDisplay = display.text!
-			display.text = textCurrentlyInDisplay + digit
+			if digit == "." && textCurrentlyInDisplay.range(of: ".") != nil {
+				display.text = textCurrentlyInDisplay
+			} else {
+				display.text = textCurrentlyInDisplay + digit
+			}
 		} else {
 			display.text = digit
 		}
