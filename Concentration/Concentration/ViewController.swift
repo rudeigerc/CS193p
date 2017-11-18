17954e23aecd2fa4bbff69101b4aac86ae65fbe3
rudeigerc <rudeigerc@gmail.com>
2017-11-19 01:34:34 +0800

    init concentration

---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Yuchen Cheng on 2017/11/19.
+//  Copyright © 2017年 Yuchen Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
